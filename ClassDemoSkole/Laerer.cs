be17cf18e4bfefbf014c8b3a4569cdfb9665b5ce
Peter Levinsky <pele@zealand.dk>
2023-10-03 14:53:32 +0200

    Add project files.

---
+++ ClassDemoSkole/Laerer.cs
@@ -0,0 +1,55 @@
+﻿namespace ClassDemoSkole
+{
+    public class Laerer
+    {
+        /*
+         * InstansFelter
+         */
+
+        private string _navn;
+        private string _uddannelse;
+        private string _viden;
+        private int _alder;
+
+        /*
+         * Properties
+         */
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+
+        public string Uddannelse
+        {
+            get { return _uddannelse; }
+            set { _uddannelse = value; }
+        }
+        public string Viden
+        {
+            get { return _viden; }
+            set { _viden = value; }
+        }
+
+        //public String GetViden()
+        //{
+        //    return _viden;
+        //}
+        //public void SetViden(string viden)
+        //{
+        //    _viden = viden;
+        //}
+
+        public int Alder
+        {
+            get { return _alder; }
+            set { _alder = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Navn)}={Navn}, {nameof(Uddannelse)}={Uddannelse}, {nameof(Viden)}={Viden}, {nameof(Alder)}={Alder.ToString()}}}";
+        }
+    }
+}
---
+++ ClassDemoSkole/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassDemoSkole;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+
+int i = 9;
+// type variabelnavn lig med værdi
+
+// samme gælder for klasser og objekter
+
+Laerer peter = new Laerer();
+peter.Navn = "Peter";
+peter.Uddannelse = "KU";
+peter.Viden = "Masser";
+peter.Alder = 65;
+
+Console.WriteLine(peter);
